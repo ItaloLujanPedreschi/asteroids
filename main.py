799bb04dae9753dbee8961609977f520100826a4
ItaloLujanPedreschi <italo.lujanpedreschi@gmail.com>
2024-09-20 16:00:49 -0600

    make player rotate

--- main.py
+++ main.py
@@ -6,25 +6,23 @@ def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   dt = 0
-  running = True
 
-  while running:
+  while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
 
     screen.fill("black")
-
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player.update(dt)
     player.draw(screen)
-
     pygame.display.flip()
     dt = clock.tick(60) / 1000
 
-  print("Starting asteroids!")
-  print("Screen width:", SCREEN_WIDTH)
-  print("Screen height:", SCREEN_HEIGHT)
+  # print("Starting asteroids!")
+  # print("Screen width:", SCREEN_WIDTH)
+  # print("Screen height:", SCREEN_HEIGHT)
 
 if __name__ == "__main__":
   main()
