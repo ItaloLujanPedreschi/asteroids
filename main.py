a2beb39b99c5b66149e673708b8bec87a296e7f3
ItaloLujanPedreschi <italo.lujanpedreschi@gmail.com>
2024-09-20 15:32:32 -0600

    setup fps cap

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  clock = pygame.time.Clock()
+  dt = 0
   running = True
 
   while running:
@@ -13,6 +15,7 @@ def main():
 
     screen.fill("black")
     pygame.display.flip()
+    dt = clock.tick(60) / 1000
 
   print("Starting asteroids!")
   print("Screen width:", SCREEN_WIDTH)
