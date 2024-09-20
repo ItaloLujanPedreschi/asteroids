691f358ac39bfb61de23da1d1ca6d4f2d1d16291
ItaloLujanPedreschi <italo.lujanpedreschi@gmail.com>
2024-09-20 15:55:42 -0600

    create and draw player on screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
   pygame.init()
@@ -14,6 +15,10 @@ def main():
         return
 
     screen.fill("black")
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player.draw(screen)
+
     pygame.display.flip()
     dt = clock.tick(60) / 1000
 
