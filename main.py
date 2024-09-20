7c2070f62421443440d8d4452554c6b51b803a01
ItaloLujanPedreschi <italo.lujanpedreschi@gmail.com>
2024-09-20 16:10:45 -0600

    create updatable and drawable groups

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
-  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   dt = 0
+
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  Player.containers = (updatable, drawable)
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
   while True:
     for event in pygame.event.get():
@@ -15,14 +19,14 @@ def main():
         return
 
     screen.fill("black")
-    player.update(dt)
-    player.draw(screen)
-    pygame.display.flip()
-    dt = clock.tick(60) / 1000
+    for obj in updatable:
+      obj.update(dt)
+    for obj in drawable:
+      obj.draw(screen)
 
-  # print("Starting asteroids!")
-  # print("Screen width:", SCREEN_WIDTH)
-  # print("Screen height:", SCREEN_HEIGHT)
+    pygame.display.flip()
+
+    dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
   main()
